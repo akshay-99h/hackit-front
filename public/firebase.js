14ece0b3ca6be4e57b55f6acd21dcaf2d7a9aee0
Akshay Prabhat Mishra <akshaypmishra02@gmail.com>
2023-03-29 06:53:02 +0530

    initial commit

---
+++ public/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD_x_M9KRsXU16ifbbhdIWRZ6O4jrfVtXA",
+  authDomain: "auth-f1c05.firebaseapp.com",
+  projectId: "auth-f1c05",
+  storageBucket: "auth-f1c05.appspot.com",
+  messagingSenderId: "744598538279",
+  appId: "1:744598538279:web:5ae3269f73f52cbbd1a602",
+  measurementId: "G-TYXPRQY6GS"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
